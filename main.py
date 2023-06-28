0d8ff27fa2d6b9063b6a210b4b6759950108ea8b
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-06-28 10:38:03 -0700

    Added goodbye message

--- main.py
+++ main.py
@@ -13,14 +13,17 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
-
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+    
+    if message.content.startswith('$bye'):
+        await message.channel.send('Good Bye')
 
 client.run(TOKEN)
